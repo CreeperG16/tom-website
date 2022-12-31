d9cba502d526ea1f1a71340d36a11c486b69b2c9
CreeperG16 <pappt1216@gmail.com>
2022-12-31 17:52:10 +0100

    webhook maybe?

--- pages/api/linear-webhook.ts
+++ pages/api/linear-webhook.ts
@@ -1,10 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-    name: string;
-};
+const webhookUrl =
+    "https://discord.com/api/webhooks/1058783560540098651/aiobWWPgzahkCPmkM36E-VTl7d3-kL8vQA4vcXt3NFNj7PNVsOHUVw8lzfaOFKwygjnq";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req.body);
-    res.status(200);
+    // console.log(req.body);
+
+    fetch(webhookUrl, {
+        method: "POST",
+        body: JSON.stringify({
+            content: JSON.stringify(req.body),
+        }),
+    }).then(() => res.status(200));
+
+    return;
 }
