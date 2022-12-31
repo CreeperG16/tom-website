118511a85ddff9bc9ceaf67cf74fa2d2c073557c
CreeperG16 <pappt1216@gmail.com>
2022-12-31 18:35:46 +0100

    some stuff

--- pages/api/linear-webhook.ts
+++ pages/api/linear-webhook.ts
@@ -7,13 +7,42 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
     if (req.method !== "POST") return res.status(400);
 
+    const embed = {
+        title: `${req.body.type} ${req.body.action}d`,
+        description: "HALLO",
+        timestamp: req.body.data.updatedAt,
+        url: req.body.url,
+    };
+
+    switch (req.body.action) {
+        case "create": {
+            if (req.body.type === "Issue") {
+                embed.description = `Issue **${req.body.data.team.key}-${req.body.data.number}**: ${req.body.data.title}`;
+                if (req.body.data.assignee) {
+                    embed.description += `\nAssigned to **${req.body.data.assignee.name}**`;
+                }
+                // embed.description += `\n`;
+            }
+            break;
+        }
+        case "update": {
+            // embed.title = `${}`
+            embed.description = "```json\n" + JSON.stringify(req.body, null, 4) + "```";
+            break;
+        }
+        case "remove": {
+            embed.description = "```json\n" + JSON.stringify(req.body, null, 4) + "```";
+            break;
+        }
+    }
+
     fetch(process.env.WEBHOOK_URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            content: "`" + typeof req.body + "`\n" + "```json\n" + JSON.stringify(req.body, null, 4) + "```",
+            embeds: [embed],
         }),
     });
 
