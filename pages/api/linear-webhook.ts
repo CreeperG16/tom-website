7f3ddaa7d12029295a895d7970efadd7bd4a7423
CreeperG16 <pappt1216@gmail.com>
2022-12-31 18:14:24 +0100

    ok now maybe

--- pages/api/linear-webhook.ts
+++ pages/api/linear-webhook.ts
@@ -5,8 +5,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
     if (!process.env.WEBHOOK_URL) return res.status(500);
 
+    if (req.method !== "POST") return res.status(400);
+
     fetch(process.env.WEBHOOK_URL, {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify({
             content: JSON.stringify(req.body),
         }),
