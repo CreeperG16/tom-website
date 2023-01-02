92874ff8fda7b103cf0fe5dfdd26af9e5fe3f5f9
CreeperG16 <pappt1216@gmail.com>
2023-01-02 18:09:07 +0100

    Show git info on home, update readme

---
+++ pages/api/gitinfo.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { execSync } from "child_process";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const commits = execSync("git log")
+        .toString()
+        .split("Author:")
+        .map((x) =>
+            x
+                .split("\n")
+                .map((y) => y.trim())
+                .filter((y) => y)
+        )
+        .map((x, i, arr) => [arr[i - 1]?.at?.(-1), ...(x.length === 4 ? x.slice(0, -1) : x)])
+        .slice(1)
+        .map((x) => ({
+            id: x[0]?.split(" ")[1].trim(),
+            message: x[3],
+            date: x[2]?.split(":").slice(1).join(":").trim?.(),
+            author: { name: x[1]?.split(" ")[0], email: x[1]?.split(" ")[1]?.replace?.(/<|>/g, "") },
+        }));
+
+    res.status(200).json(commits);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,62 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [commitInfo, setCommitInfo] = useState({ id: "", message: "", date: "", author: { name: "", email: "" } });
+
+    useEffect(() => {
+        // const commits = await fetch("https://tom-website.vercel.app/api/gitinfo").then((x) => x.json());
+        fetch("http://localhost:3000/api/gitinfo")
+            .then((x) => x.json())
+            .then((x) => setCommitInfo(x[0]));
+        console.log(1);
+    }, []);
+
     return (
         <>
             <Head>
                 <title>Tom16</title>
             </Head>
-            <main
-                style={{
-                    position: "absolute",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                }}
-            >
-                <h1 style={{ fontFamily: "sans-serif" }}>hellooo</h1>
+            <main>
+                <div
+                    style={{
+                        position: "absolute",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                    }}
+                >
+                    <h1 style={{ fontFamily: "sans-serif" }}>hellooo</h1>
+                </div>
+                <footer
+                    style={{
+                        position: "absolute",
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "right",
+                        padding: 15,
+                    }}
+                >
+                    <p style={{ fontFamily: "sans-serif", opacity: 0.5 }}>
+                        Commit{" "}
+                        <a
+                            href={"https://github.com/CreeperG16/tom-website/commit/" + commitInfo.id}
+                            title={commitInfo.date}
+                            target="_blank"
+                        >
+                            {commitInfo.id}
+                        </a>{" "}
+                        <span style={{ fontStyle: "italic" }}>{commitInfo.message}</span> - {commitInfo.author.name}
+                    </p>
+                </footer>
             </main>
         </>
     );
