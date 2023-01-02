92e94ea6a6cf2b3f4326e322d17bcfb6f7cd0c05
CreeperG16 <pappt1216@gmail.com>
2023-01-02 20:27:03 +0100

    some more animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import Head from "next/head";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Home() {
     const commitInfo = JSON.parse(process.env.NEXT_PUBLIC_GIT_COMMITS ?? "[]")[0];
+
+    const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+        typeof window !== "undefined" && setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    }, []);
 
     return (
         <>
@@ -12,14 +19,11 @@ export default function Home() {
             <main>
                 <div
                     style={{
-                        position: "absolute",
                         display: "flex",
                         alignItems: "center",
                         justifyContent: "center",
-                        top: 0,
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
+                        height: windowSize.height,
+                        width: windowSize.width,
                     }}
                 >
                     <motion.div
@@ -34,15 +38,87 @@ export default function Home() {
                             width: 200,
                             height: 100,
                             borderRadius: 15,
-                            backgroundColor: "#5865F2",
+                            backgroundColor: "#03c59ade",
+                            boxShadow: "5px 5px 15px #ffffff55",
                         }}
                     >
-                        <h1 style={{ fontFamily: "sans-serif" }}>hellooo</h1>
+                        <h1>hellooo</h1>
                     </motion.div>
                 </div>
-                <footer
+
+                <div
                     style={{
-                        position: "absolute",
+                        width: windowSize.width,
+                        height: 400,
+                        backgroundColor: "#03c59a5e",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <motion.div
+                        animate
+                        initial={{ x: windowSize.width / 2, opacity: 0 }}
+                        whileInView={{ x: -(windowSize.width / 2) + 150, opacity: 1 }}
+                        transition={{ delay: 0 }}
+                        style={{
+                            width: 200,
+                            height: 100,
+                            backgroundColor: "#C51B46bb",
+                            borderRadius: 10,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            margin: 5,
+                            boxShadow: "5px 5px 15px #00000055",
+                        }}
+                    >
+                        <h2>one</h2>
+                    </motion.div>
+                    <motion.div
+                        animate
+                        initial={{ x: windowSize.width / 2, opacity: 0 }}
+                        whileInView={{ x: -(windowSize.width / 2) + 150, opacity: 1 }}
+                        transition={{ delay: 0.5 }}
+                        style={{
+                            width: 200,
+                            height: 100,
+                            backgroundColor: "#3775D2bb",
+                            borderRadius: 10,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            margin: 5,
+                            boxShadow: "5px 5px 15px #00000055",
+                        }}
+                    >
+                        <h2>two</h2>
+                    </motion.div>
+                    <motion.div
+                        animate
+                        initial={{ x: windowSize.width / 2, opacity: 0 }}
+                        whileInView={{ x: -(windowSize.width / 2) + 150, opacity: 1 }}
+                        transition={{ delay: 1 }}
+                        style={{
+                            width: 200,
+                            height: 100,
+                            backgroundColor: "#F4D35Ebb",
+                            borderRadius: 10,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            margin: 5,
+                            boxShadow: "5px 5px 15px #00000055",
+                        }}
+                    >
+                        <h2>three</h2>
+                    </motion.div>
+                </div>
+
+                <div
+                    style={{
+                        position: "fixed",
                         bottom: 0,
                         left: 0,
                         right: 0,
@@ -63,7 +139,7 @@ export default function Home() {
                         </a>{" "}
                         <span style={{ fontStyle: "italic" }}>{commitInfo?.message}</span> - {commitInfo?.author?.name}
                     </p>
-                </footer>
+                </div>
             </main>
         </>
     );
