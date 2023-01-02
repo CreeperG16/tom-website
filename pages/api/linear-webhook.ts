37e30633f2cf9423ad4d1671258954aaaabc11c6
CreeperG16 <pappt1216@gmail.com>
2023-01-02 15:56:28 +0100

    this might work?

--- pages/api/linear-webhook.ts
+++ pages/api/linear-webhook.ts
@@ -1,33 +1,94 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { LinearClient } from "@linear/sdk";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+type DiscordEmbed = {
+    title?: string;
+    type?: "rich" | "image" | "video" | "gifv" | "article" | "link";
+    description: string;
+    url?: string;
+    timestamp?: string;
+    color?: number;
+    footer?: {
+        text: string;
+        icon_url?: string;
+        proxy_icon_url?: string;
+    };
+    image?: {
+        url: string;
+        proxy_url?: string;
+        height?: number;
+        width?: number;
+    };
+    thumbnail?: {
+        url: string;
+        proxy_url?: string;
+        height?: number;
+        width?: number;
+    };
+    video?: {
+        url: string;
+        proxy_url?: string;
+        height?: number;
+        width?: number;
+    };
+    provider?: {
+        name?: string;
+        url?: string;
+    };
+    author?: {
+        name: string;
+        url?: string;
+        icon_url?: string;
+        proxy_icon_url?: string;
+    };
+    fields?: { name: string; value: string; inline?: boolean }[];
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // console.log(req.body);
 
     if (!process.env.WEBHOOK_URL) return res.status(500);
 
     if (req.method !== "POST") return res.status(400);
 
-    const embed = {
-        title: `${req.body.type} ${req.body.action}d`,
-        description: "HALLO",
+    // Api key authentication
+    const client = new LinearClient({
+        apiKey: process.env.LINEAR_API_KEY,
+    });
+
+    const author = await client.user(req.body.data.creatorId);
+
+    const embed: DiscordEmbed = {
+        title: `${req.body.type} ${req.body.data.team.key}-${req.body.data.number} ${req.body.action}d - ${req.body.data.title}`,
+        description: req.body.data.description,
         timestamp: req.body.data.updatedAt,
         url: req.body.url,
+        author: {
+            name: author.displayName,
+            icon_url: author.avatarUrl,
+            url: author.url,
+        },
+        footer: {
+            text: req.body.data.project?.name,
+        },
     };
 
     switch (req.body.action) {
         case "create": {
             if (req.body.type === "Issue") {
-                embed.description = `Issue **${req.body.data.team.key}-${req.body.data.number}**: ${req.body.data.title}`;
                 if (req.body.data.assignee) {
                     embed.description += `\nAssigned to **${req.body.data.assignee.name}**`;
                 }
-                // embed.description += `\n`;
             }
             break;
         }
         case "update": {
             // embed.title = `${}`
             embed.description = "```json\n" + JSON.stringify(req.body, null, 4) + "```";
+            embed.fields = Object.entries(req.body.updatedFrom).map(([k, v]) => ({
+                name: k,
+                value: `${v} -> ${req.body.data[k]}`,
+            }));
             break;
         }
         case "remove": {
@@ -36,7 +97,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         }
     }
 
-    fetch(process.env.WEBHOOK_URL, {
+    await fetch(process.env.WEBHOOK_URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
