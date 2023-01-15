78792a2cef6112733142e5db373d7b6194430332
CreeperG16 <pappt1216@gmail.com>
2023-01-15 18:33:44 +0100

    haladás

--- pages/nyelvtan.tsx
+++ pages/nyelvtan.tsx
@@ -11,8 +11,59 @@ export default function Login() {
                 </div>
             </div>
             <div id="two">
-                <h1 className="info">a</h1>
-                <h1 className="info">a</h1>
+                <h1 className="info">
+                    Az erdőirtás folyamata egy olyan egzisztenciális veszély mely a köztudatban eltörpül a klímaváltozás
+                    kérdése mellett, de a kettő összeköthető. Definiálva az erdőirtás az a fakitermelés folyamata
+                    újratelepítési és erdőgazdálkodási tervek nélkül. Ebből származóan az erdőirtás szinte egyidős az
+                    emberiséggel.
+                </h1>
+                <h1 className="section-title">Mit is értünk erdőirtás alatt?</h1>
+            </div>
+            <div id="three">
+                <h1 className="section-title">Miért történik?</h1>
+                <h1 className="info">
+                    Az oka a jelen időnkben származhat a fa, mint termék, korlátozatlan kitermelése, urbanizáció
+                    növekedése és a mezőgazdasági területek bővítéséből.
+                </h1>
+            </div>
+            <div id="four">
+                <h1 className="info">
+                    A következményei a folyamatnak több helyen is megnyilvánulnak. Kezdve a légkörrel, az erdők és a
+                    benne elő mikroorganizmusok teszik lehetővé a levegő megújulását, így fonódik össze a globális
+                    felmelegedés kérdésével. Emellett az erdők részt vesznek a talaj megkötésében mely omladozó, szirtes
+                    területeken kifejezetten fontos, vagy például segítik a Szahara terjedését és a sivatagosodás
+                    folyamatának lassítását. Ezen kívül az erdők számtalan állatoknak adnak otthon melyek az előterük
+                    csökkenésével a kihalás szélére kerülhetnek.
+                </h1>
+                <h1 className="section-title">A következmények</h1>
+            </div>
+            <div id="five">
+                <h1 className="section-title">Mit tehetünk?</h1>
+                <h1 className="info">
+                    Magán személyként eldobható papírtermékek kerülése lehet egy lépés melyet könnyű meglépni. Ezentúl a
+                    szerveződésben, a kampányolásban, petíciókban vagy zöldebb pártok megválasztásában nyilvánulhatnak
+                    meg törekvéseink. Továbbá a technológia fejlődésével a talaj regenerálása és erdők újraültetése
+                    egyre könnyebbé válik.
+                </h1>
+            </div>
+            <div id="six">
+                <h1 className="info">
+                    Az egyik ország melyre példaként tekinthetünk ebben az Brazília. A korábbi elnök, Jair Bolsonaro,
+                    nemhogy csak nem tett lépéseket az Amazonas irtásának megállításáért, hanem elősegítette, viszont a
+                    nemrég lezajlott választások során Luiz Inácio Lula da Silva került ki nyertesként, az elődjéhez
+                    képest egy nagyon zöld tervel futva.
+                </h1>
+                <h1 className="section-title">Brazília</h1>
+            </div>
+            <div id="seven">
+                <h1 className="section-title">Hazánk</h1>
+                <h1 className="info">
+                    Régebben hazánk mai területének több mint 2/3-át sűrű erdők borították, mely mára csak 19,6%-ra
+                    csökkent a folytonos fakitermelésnek köszönhetően. A WFF 2016-ban kiadott cikke szerint,
+                    Magyarországon a 180 évnél idősebb erdőterületek terjedelme nem éri el az 1000 hektárt. Míg
+                    országunkban kifejezett törekvések az újraerdősítés elősegítésében ritkák, az erdős területek száma
+                    lassan de biztosan nő, a mesterséges erdők ültetésének köszönhetően.
+                </h1>
             </div>
         </div>
     );
