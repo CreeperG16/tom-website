f0216a3abddef0da4eaa883c048303f59b92b61d
Tamas Papp <86970314+CreeperG16@users.noreply.github.com>
2023-03-31 13:18:13 +0200

    center etc

--- pages/szeretnek-jatszani.tsx
+++ pages/szeretnek-jatszani.tsx
@@ -1,7 +1,9 @@
 export default function SzeretnekJatszani() {
   //@ts-ignore
-  return (<div>
+  return (<div style="display: flex; align-items: center; justify-content: center;">
       <h1>Szeretnék Játszani!</h1>
+      // quick and dirty spacing
+      <br />
       <br />
       <img src="https://www.minecraft.net/content/dam/games/minecraft/logos/logo-minecraft.svg" />
     </div>)
