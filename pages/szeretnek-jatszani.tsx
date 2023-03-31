342ff9df892e9ec972bd60894f7f3b44a3e8bb33
Tamas Papp <86970314+CreeperG16@users.noreply.github.com>
2023-03-31 13:24:21 +0200

    eh

--- pages/szeretnek-jatszani.tsx
+++ pages/szeretnek-jatszani.tsx
@@ -1,7 +1,12 @@
 export default function SzeretnekJatszani() {
   //@ts-ignore
   return (
-    <div style={{display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+    <div style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+    }}>
       <h1>Szeretnék Játszani!</h1>
       {/* quick and dirty spacing*/}
       <br />
