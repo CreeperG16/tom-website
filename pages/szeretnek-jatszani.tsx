63dac7f19c37b8708078bc58c3a7e1064aee77c7
Tamas Papp <86970314+CreeperG16@users.noreply.github.com>
2023-03-31 13:21:31 +0200

    comment

--- pages/szeretnek-jatszani.tsx
+++ pages/szeretnek-jatszani.tsx
@@ -1,10 +1,12 @@
 export default function SzeretnekJatszani() {
   //@ts-ignore
-  return (<div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+  return (
+    <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
       <h1>Szeretnék Játszani!</h1>
-      // quick and dirty spacing
+      {/* quick and dirty spacing*/}
       <br />
       <br />
       <img src="https://www.minecraft.net/content/dam/games/minecraft/logos/logo-minecraft.svg" />
-    </div>)
+    </div>
+  );
 }
