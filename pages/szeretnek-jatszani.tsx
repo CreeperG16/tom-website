1ba5a1838f8517be690ea13cbbb793f0f9921acd
Tamas Papp <86970314+CreeperG16@users.noreply.github.com>
2023-03-31 13:09:20 +0200

    br /

--- pages/szeretnek-jatszani.tsx
+++ pages/szeretnek-jatszani.tsx
@@ -1,4 +1,8 @@
 export default function SzeretnekJatszani() {
   //@ts-ignore
-  return (<><h1>Szeretnék Játszani!</h1><br><img src="https://www.minecraft.net/content/dam/games/minecraft/logos/logo-minecraft.svg" /></>)
+  return (<div>
+      <h1>Szeretnék Játszani!</h1>
+      <br />
+      <img src="https://www.minecraft.net/content/dam/games/minecraft/logos/logo-minecraft.svg" />
+    </div>)
 }
